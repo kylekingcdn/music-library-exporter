3663a48a0d393134b0e880f33bb496e9a857273b
Kyle King <kyle.king@shaw.ca>
2021-01-18 17:17:58 -0800

    Initial xcodeproj commit

---
+++ itunesxmlgen/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  itunesxmlgen
+//
+//  Created by Kyle King on 2021-01-18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+    
+  }
+  return 0;
+}
