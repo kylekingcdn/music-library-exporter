8292632f5afd65fb3383468a958e0da2da196f5a
Kyle King <kyle.king@shaw.ca>
2021-02-22 22:48:55 -0800

    Disable library-generator music dir exists check

--- library-generator/LibraryGenerator.m
+++ library-generator/LibraryGenerator.m
@@ -297,25 +297,25 @@
     return NO;
   }
 
-  NSFileManager* fileManager = [NSFileManager defaultManager];
-
-  BOOL pathIsDirectory;
-  BOOL pathExists = [fileManager fileExistsAtPath:musicDirPath isDirectory:&pathIsDirectory];
-
-  // ensure specified file path is writable
-  if (!pathExists) {
-    *error = [NSError errorWithDomain:__MLE_ErrorDomain_LibraryGenerator code:LibraryGeneratorErrorInvalidMusicMediaDirectory userInfo:@{
-      NSLocalizedDescriptionKey:[NSString stringWithFormat:@"Error: The specified music media directory does not exist: %@", musicDirPath],
-    }];
-    return NO;
-  }
-
-  if (!pathIsDirectory) {
-    *error = [NSError errorWithDomain:__MLE_ErrorDomain_LibraryGenerator code:LibraryGeneratorErrorInvalidMusicMediaDirectory userInfo:@{
-      NSLocalizedDescriptionKey:[NSString stringWithFormat:@"Error: The specified music media location is not a directory: %@. Please copy and paste the exact value from Music Preferences. More information can be found by running 'library-generate help'.", musicDirPath],
-    }];
-    return NO;
-  }
+//  NSFileManager* fileManager = [NSFileManager defaultManager];
+//
+//  BOOL pathIsDirectory;
+//  BOOL pathExists = [fileManager fileExistsAtPath:musicDirPath isDirectory:&pathIsDirectory];
+//
+//  // ensure specified file path is writable
+//  if (!pathExists) {
+//    *error = [NSError errorWithDomain:__MLE_ErrorDomain_LibraryGenerator code:LibraryGeneratorErrorInvalidMusicMediaDirectory userInfo:@{
+//      NSLocalizedDescriptionKey:[NSString stringWithFormat:@"Error: The specified music media directory does not exist: %@", musicDirPath],
+//    }];
+//    return NO;
+//  }
+//
+//  if (!pathIsDirectory) {
+//    *error = [NSError errorWithDomain:__MLE_ErrorDomain_LibraryGenerator code:LibraryGeneratorErrorInvalidMusicMediaDirectory userInfo:@{
+//      NSLocalizedDescriptionKey:[NSString stringWithFormat:@"Error: The specified music media location is not a directory: %@. Please copy and paste the exact value from Music Preferences. More information can be found by running 'library-generate help'.", musicDirPath],
+//    }];
+//    return NO;
+//  }
 
   return YES;
 }
