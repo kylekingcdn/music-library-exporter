5aaca6de66c9adf15bb893e28f3c47f43b557a72
Kyle King <kyle.king@shaw.ca>
2022-10-27 05:30:35 -0700

    Fix OrderedDictionary property list DOCTYPE
    Relates to #74

--- 3rd/OrderedDictionary.m
+++ 3rd/OrderedDictionary.m
@@ -312,7 +312,7 @@
 - (NSString *)XMLPlistString
 {
     return [NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-            "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n"
+            "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n"
             "<plist version=\"1.0\">\n%@\n</plist>\n",
             [self XMLPlistStringWithIndent:@""]];
 }
