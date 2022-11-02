8ed4b556ad90ec33de710c7763d193e946cdb009
Kyle King <kyle.king@shaw.ca>
2022-11-02 02:21:29 -0700

    Add placeholder MediaItemSorter class

---
+++ common/Sorter/MediaItemSorter.h
@@ -0,0 +1,19 @@
+//
+//  MediaItemSorter.h
+//  Music Library Exporter
+//
+//  Created by Kyle King on 2022-11-01.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MediaItemSorter : NSObject {
+
+}
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ common/Sorter/MediaItemSorter.m
@@ -0,0 +1,12 @@
+//
+//  MediaItemSorter.m
+//  Music Library Exporter
+//
+//  Created by Kyle King on 2022-11-01.
+//
+
+#import "MediaItemSorter.h"
+
+@implementation MediaItemSorter
+
+@end
