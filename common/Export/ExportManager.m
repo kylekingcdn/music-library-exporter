781c0cc43225f2e3896ee6a2eb87babd43c4be66
Kyle King <kyle.king@shaw.ca>
2022-11-11 18:16:18 -0800

    Disable ExportManagerDelegate logging

--- common/Export/ExportManager.m
+++ common/Export/ExportManager.m
@@ -269,7 +269,7 @@ NSErrorDomain const __MLE_ErrorDomain_ExportManager = @"com.kylekingcdn.MusicLib
 - (void)serializedItems:(NSUInteger)serialized ofTotal:(NSUInteger)total {
 
   if (serialized % 10 == 0 || serialized == total) {
-    MLE_Log_Info(@"ExportManager [serializedItems] serializing items %lu/%lu", (unsigned long)serialized, total);
+//    MLE_Log_Info(@"ExportManager [serializedItems] serializing items %lu/%lu", (unsigned long)serialized, total);
 
     if (_delegate != nil && [_delegate respondsToSelector:@selector(exportedItems:ofTotal:)]) {
       [_delegate exportedItems:serialized ofTotal:total];
@@ -282,7 +282,7 @@ NSErrorDomain const __MLE_ErrorDomain_ExportManager = @"com.kylekingcdn.MusicLib
 
 - (void)serializedPlaylists:(NSUInteger)serialized ofTotal:(NSUInteger)total {
 
-  MLE_Log_Info(@"ExportManager [serializedPlaylists] serializing playlists %lu/%lu", serialized, total);
+//  MLE_Log_Info(@"ExportManager [serializedPlaylists] serializing playlists %lu/%lu", serialized, total);
 
   if (_delegate != nil && [_delegate respondsToSelector:@selector(exportedPlaylists:ofTotal:)]) {
     [_delegate exportedPlaylists:serialized ofTotal:total];
