a8ca31c77ef61e8d8cf72ca4d4d256f7e5e62c51
Kyle King <kyle.king@shaw.ca>
2022-11-18 03:13:06 -0800

    Add MediaItemSorter fallback/substitution support
    Relates to #89

--- src/Common/Sorter/MediaItemSorter.m
+++ src/Common/Sorter/MediaItemSorter.m
@@ -7,6 +7,7 @@
 
 #import "MediaItemSorter.h"
 
+#import <iTunesLibrary/ITLibAlbum.h>
 #import <iTunesLibrary/ITLibArtist.h>
 #import <iTunesLibrary/ITLibMediaItem.h>
 
@@ -21,13 +22,18 @@
 - (nullable id)valueOfItem:(ITLibMediaItem*)item forProperty:(NSString*)property;
 - (nullable id)preprocessValue:(nullable id)value forProperty:(NSString*)property;
 
-+ (NSComparisonResult)alphabeticallyCompareString:(NSString*)str1 withString:(NSString*)str2;
+- (NSComparisonResult)compareItem:(ITLibMediaItem*)item1 withItem:(ITLibMediaItem*)item2;
+- (NSComparisonResult)compareProperty:(NSString*)property ofItem:(ITLibMediaItem*)item1 withItem:(ITLibMediaItem*)item2 order:(PlaylistSortOrderType)order;
+
+- (NSComparisonResult)alphabeticallyCompareString:(NSString*)str1 withString:(NSString*)str2;
 
 @end
 
 @implementation MediaItemSorter {
 
-  NSArray<NSString*>* _sortPrefixes;
+  NSString* _sortProperty; // temporary until PlaylistSortColumnType is migrated to property strings
+
+  NSArray<NSString*>* _sortPrefixes; // temp
 }
 
 #pragma mark - Initializers
@@ -44,6 +50,11 @@
     _sortColumn = sortColumn;
     _sortOrder = sortOrder;
 
+    _sortProperty = [Utils mediaItemPropertyForSortColumn:sortColumn]; // temp
+    if (_sortProperty == nil) {
+      _sortProperty = @"";
+    }
+
     _sortPrefixes = @[ @"a", @"an", @"the", ]; // temp
 
     return self;
@@ -55,10 +66,56 @@
 
 #pragma mark - Accessors
 
+- (NSArray<ITLibMediaItem*>*)sortItems:(NSArray<ITLibMediaItem*>*)items {
+
+  // don't sort if sort column is null
+  if (_sortColumn == PlaylistSortColumnNull) {
+    return items;
+  }
+
+  // default to ascending sort order
+  if (_sortOrder == PlaylistSortOrderNull) {
+    _sortOrder = PlaylistSortOrderAscending;
+  }
+
+
+  return [items sortedArrayUsingComparator:^NSComparisonResult(id item1, id item2) {
+    return [self compareItem:item1 withItem:item2];
+  }];
+}
+
 - (nullable id)valueOfItem:(ITLibMediaItem*)item forProperty:(NSString*)property {
 
-  return [self preprocessValue:[item valueForProperty:property] forProperty:property];
+  id itemValue;
 
+  if ([[SorterDefines propertySubstitutions] objectForKey:property] != nil) {
+
+    // if substitutions exist for the property, return the first non-empty value
+    for (NSString* substituteProperty in [SorterDefines substitutionsForProperty:property]) {
+
+      itemValue = [item valueForProperty:substituteProperty];
+
+      // return first non-empty value
+      if (itemValue != nil) {
+        /* DEBUG
+        if (substituteProperty != property) {
+          MLE_Log_Info(@"MediaItemSorter [valueOfItem] using substitute %@ for property %@ ('%@ - %@') ('%@ -> %@')", substituteProperty, property, item.album.albumArtist, item.title, [item valueForProperty:property], [item valueForProperty:substituteProperty]);
+        }
+         */
+        // re-assign property for correct pre-processing of the substituted property
+        property = substituteProperty;
+        break;
+      }
+    }
+  }
+
+  // directly return value for given property
+  else {
+    itemValue = [item valueForProperty:property];
+  }
+
+  // preprocess and return the value
+  return [self preprocessValue:itemValue forProperty:property];
 }
 
 - (nullable id)preprocessValue:(nullable id)value forProperty:(NSString*)property {
@@ -94,57 +151,64 @@
   return value;
 }
 
-- (NSArray<ITLibMediaItem*>*)sortItems:(NSArray<ITLibMediaItem*>*)items {
+- (NSComparisonResult)compareItem:(ITLibMediaItem*)item1 withItem:(ITLibMediaItem*)item2 {
 
-  // don't sort if sort column is null
-  if (_sortColumn == PlaylistSortColumnNull) {
-    return items;
+  NSComparisonResult result = [self compareProperty:_sortProperty ofItem:item1 withItem:item2 order:_sortOrder];
+
+  // values are identical, attempt to sort by fallback properties
+  if (result == NSOrderedSame) {
+
+    for (NSString* fallbackProperty in [SorterDefines fallbackPropertiesForProperty:_sortProperty]) {
+
+      // use ascending order for fallback properties
+      result = [self compareProperty:fallbackProperty ofItem:item1 withItem:item2 order:PlaylistSortOrderAscending];
+
+      // use first result that is not equal
+      if (result != NSOrderedSame) {
+//        MLE_Log_Info(@"MediaItemSorter [compareItem] used fallback %@ for property %@ ('%@ - %@', '%@ - %@')", fallbackProperty, _sortProperty, item1.album.albumArtist, item1.title, item2.album.albumArtist, item2.title);
+        break;
+      }
+    }
   }
 
-  // default to ascending sort order
-  if (_sortOrder == PlaylistSortOrderNull) {
-    _sortOrder = PlaylistSortOrderAscending;
-  }
-
-  NSString* itemProperty = [Utils mediaItemPropertyForSortColumn:_sortColumn];
-
-  return [items sortedArrayUsingComparator:^NSComparisonResult(id item1, id item2) {
-
-    id item1Value = [self valueOfItem:item1 forProperty:itemProperty];
-    id item2Value = [self valueOfItem:item2 forProperty:itemProperty];
-
-    if (item1Value == nil || item2Value == nil) {
-      if (item1Value == item2Value) {
-        return NSOrderedSame;
-      }
-      else if (item1Value) {
-        return NSOrderedAscending;
-      }
-      else {
-        return NSOrderedDescending;
-      }
-    }
-
-    NSComparisonResult result;
-
-    if (item1Value && [item1Value isKindOfClass:[NSString class]]) {
-      // TODO: handle title/sortTitle artist/sortArtist albumArtist/sortAlbumArtist
-      result = [MediaItemSorter alphabeticallyCompareString:item1Value withString:item2Value];
-    }
-    else {
-      result = [item1Value compare:item2Value];
-    }
-
-    if (_sortOrder == PlaylistSortOrderAscending) {
-      return result;
-    }
-    else {
-      return -result;
-    }
-  }];
+  return result;
 }
 
-+ (NSComparisonResult)alphabeticallyCompareString:(NSString*)str1 withString:(NSString*)str2 {
+- (NSComparisonResult)compareProperty:(NSString*)property ofItem:(ITLibMediaItem*)item1 withItem:(ITLibMediaItem*)item2 order:(PlaylistSortOrderType)order {
+
+  id item1Value = [self valueOfItem:item1 forProperty:property];
+  id item2Value = [self valueOfItem:item2 forProperty:property];
+
+  // handle nil values
+  if (item1Value == nil || item2Value == nil) {
+    if (item1Value == item2Value) {
+      return NSOrderedSame;
+    }
+    else if (item1Value) {
+      return NSOrderedAscending;
+    }
+    else {
+      return NSOrderedDescending;
+    }
+  }
+
+  NSComparisonResult result;
+  if (item1Value && [item1Value isKindOfClass:[NSString class]]) {
+    result = [self alphabeticallyCompareString:item1Value withString:item2Value];
+  }
+  else {
+    result = [item1Value compare:item2Value];
+  }
+
+  if (order == PlaylistSortOrderAscending) {
+    return result;
+  }
+  else {
+    return -result;
+  }
+}
+
+- (NSComparisonResult)alphabeticallyCompareString:(NSString*)str1 withString:(NSString*)str2 {
 
   // sort so that strings that begin with letters come before non-letter strings (begin with digit, special char, etc)
   BOOL str1LetterPrefix = [[NSCharacterSet letterCharacterSet] characterIsMember:[str1 characterAtIndex:0]];
