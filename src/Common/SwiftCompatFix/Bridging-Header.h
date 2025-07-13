6ec74c825414ed6eb1e06d192f78b8f3b96c17ae
Kyle King <kyle.king@shaw.ca>
2025-07-13 14:25:58 -0700

    Add swift compat fix for required sentry update
    Swift compat fix 2

---
+++ src/Common/SwiftCompatFix/Bridging-Header.h
@@ -0,0 +1,1 @@
+// This file is a workaround for `Could not find or use auto-linked library 'swiftCompatibility..': library 'swiftCompatibility..' not found` build failures.
---
+++ src/Common/SwiftCompatFix/Empty.swift
@@ -0,0 +1,10 @@
+//
+//  Empty.swift
+//  Music Library Exporter
+//
+//  Created by Kyle King on 2025-07-13.
+//
+
+import Foundation
+
+// This file is a workaround for `Could not find or use auto-linked library 'swiftCompatibility..': library 'swiftCompatibility..' not found` build failures.
